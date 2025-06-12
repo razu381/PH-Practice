02dec389f764d8a41f2a7cb7a4ce80b04971cd99
razu381 <sirazu52@gmail.com>
2025-06-12 19:11:59 +0600

    practice problems solved

--- RDMS/postgress/new table.sql
+++ RDMS/postgress/new table.sql
@@ -18,7 +18,7 @@
 
 -- create table students(
 --     student_id SERIAL PRIMARY KEY,
---     firstName VARCHAR(50),
+--     firstName VARCHAR(50) NOT NULL,
 --     lastName VARCHAR(50),
 --     age INT,
 --     grade VARCHAR(2),
@@ -33,27 +33,27 @@
 
 -- alter table students alter lastName set NOT null;  
 
--- insert into  students(firstname,lastname,age,grade,course,email,dob,blood_group,country)
--- values ('Alice','Smith',19,'A','Physics','alice.smith@example.com','2005-03-15','O+','USA'),
--- ('Bob','Johnson',21,'B+','Chemistry','bob.j@example.com','2003-07-22','A-','Canada'),
--- ('Charlie','Brown',20,'A+','Computer Science','charlie.b@example.com','2004-11-01','B+','UK'),
--- ('Diana','Miller',18,'B','Literature','diana.m@example.com','2006-01-28','AB-','Australia'),
--- ('Ethan','Davis',22,'C+','Economics','ethan.d@example.com','2002-09-10','O-','Germany'),
--- ('Fiona','Garcia',19,'A-','Biology','fiona.g@example.com','2005-04-03','A+','Spain'),
--- ('George','Rodriguez',23,'B','History','george.r@example.com','2001-02-19','B-','Mexico'),
--- ('Hannah','Martinez',20,'A','Art History','hannah.m@example.com','2004-06-07','O+','France'),
--- ('Ivan','Hernandez',21,'C','Mathematics','ivan.h@example.com','2003-12-25','AB+','Brazil'),
--- ('Julia','Lopez',18,'A+','Psychology','julia.l@example.com','2006-08-11','A+','Italy'),
--- ('Kevin','Gonzalez',24,'B+','Engineering','kevin.g@example.com','2000-10-05','B+','India'),
--- ('Laura','Perez',19,'B','Sociology','laura.p@example.com','2005-01-09','O-','Argentina'),
--- ('Michael','Wilson',20,'A-','Political Science','michael.w@example.com','2004-05-30','A-','South Africa'),
--- ('Nora','Taylor',22,'C+','Graphic Design','nora.t@example.com','2002-03-02','B-','Japan'),
--- ('Oscar','Anderson',21,'A','Data Science','oscar.a@example.com','2003-09-18','O+','Sweden'),
--- ('Paula','Thomas',19,'B+','Nursing','paula.t@example.com','2005-07-04','AB+','Philippines'),
--- ('Quinn','Jackson',20,'A+','Philosophy','quinn.j@example.com','2004-02-29','A+','Ireland'),
--- ('Rachel','White',18,'B','Music','rachel.w@example.com','2006-11-14','B+','New Zealand'),
--- ('Sam','Harris',23,'C','Finance','sam.h@example.com','2001-06-01','O-','Egypt'),
--- ('Tina','Clark',20,'A','Architecture','tina.c@example.com','2004-09-21','A-','Turkey')
+insert into  students(firstname,lastname,age,grade,course,email,dob,blood_group,country)
+values ('Alice','Smith',19,'A','Physics','alice.smith@example.com','2005-03-15','O+','USA'),
+('Bob','Johnson',21,'B+','Chemistry',null,'2003-07-22','A-','Canada'),
+('Charlie','Brown',20,'A+','Computer Science','charlie.b@example.com','2004-11-01','B+','UK'),
+('Diana','Miller',18,'B','Literature',null,'2006-01-28','AB-','Australia'),
+('Ethan','Davis',22,'C+','Economics','ethan.d@example.com','2002-09-10','O-','Germany'),
+('Fiona','Garcia',19,'A-','Biology','fiona.g@example.com','2005-04-03','A+','Spain'),
+('George','Rodriguez',23,'B','History','george.r@example.com','2001-02-19','B-','Mexico'),
+('Hannah','Martinez',20,'A','Art History','hannah.m@example.com','2004-06-07','O+','France'),
+('Ivan','Hernandez',21,'C','Mathematics','ivan.h@example.com','2003-12-25','AB+','Brazil'),
+('Julia','Lopez',18,'A+','Psychology','julia.l@example.com','2006-08-11','A+','Italy'),
+('Kevin','Gonzalez',24,'B+','Engineering','kevin.g@example.com','2000-10-05','B+','India'),
+('Laura','Perez',19,'B','Sociology','laura.p@example.com','2005-01-09','O-','Argentina'),
+('Michael','Wilson',20,'A-','Political Science','michael.w@example.com','2004-05-30','A-','South Africa'),
+('Nora','Taylor',22,'C+','Graphic Design','nora.t@example.com','2002-03-02','B-','Japan'),
+('Oscar','Anderson',21,'A','Data Science','oscar.a@example.com','2003-09-18','O+','Sweden'),
+('Paula','Thomas',19,'B+','Nursing','paula.t@example.com','2005-07-04','AB+','Philippines'),
+('Quinn','Jackson',20,'A+','Philosophy','quinn.j@example.com','2004-02-29','A+','Ireland'),
+('Rachel','White',18,'B','Music','rachel.w@example.com','2006-11-14','B+','New Zealand'),
+('Sam','Harris',23,'C','Finance','sam.h@example.com','2001-06-01','O-','Egypt'),
+('Tina','Clark',20,'A','Architecture','tina.c@example.com','2004-09-21','A-','Turkey')
 
 
 -- select email,age,firstname from students
@@ -83,4 +83,52 @@
 --scaler function 
 select concat_ws(' ',firstname,lastname) from students  age
 
+SELECT LENGTH(firstname) FROM students;
 
+select now() 
+
+select avg(age) from students
+where age < 20 
+
+select min(age) from students 
+
+select max(age ) from students 
+
+select sum(age) from students 
+
+--we can combine both scaler and aggregrate functions 
+select max(length(firstname)) from students age
+
+select * from students
+    WHERE email is not NULL
+
+select COALESCE(email, 'mail not provided')  from students
+
+select * from students
+    where country = 'USA' or country = 'Canada' or country = 'Turkey'
+
+SELECT * FROM students
+    WHERE country IN('USA','Canada','Turkey')
+
+SELECT * FROM students
+    WHERE dob BETWEEN '2001-01-01' and '2002-12-01' ORDER BY dob ASC
+
+SELECT * FROM students
+    WHERE firstname LIKE '%on%'
+
+SELECT * FROM students
+    WHERE firstname ILIKE 'sam'
+
+
+SELECT * FROM students LIMIT 5 OFFSET 5
+
+
+TRUNCATE students
+
+
+UPDATE students
+    set email = 'dontknow@gmail.com'
+    where student_id=22
+
+DELETE from students
+    WHERE email='notknownyet@gmail.com'
